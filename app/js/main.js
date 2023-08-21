caf0149347b2838fa75a66e29ae3c9da394cadf0
Alex090283 <alex.dev090283@gmail.com>
2023-08-21 08:50:22 +0300

    block cover finish

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,9 @@ $(function(){
     $('.cover__slider').slick({
         prevArrow: '<button class="slider-btn slider-btn__left"></button>',
         nextArrow: '<button class="slider-btn slider-btn__right"></button>',
+        autoplay: true,
+        dots: true,
+        fade: true,
     });
 
 });
